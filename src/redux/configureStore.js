ec6669005a96a3597b14899963d9df37332a403b
admirerbrown <admirerbrown@gmail.com>
2023-01-24 08:33:09 +0000

    implement category requirement

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
+import categoryReducer from './categories/categories';
 
 const store = configureStore({
   reducer: {
     book: bookReducer,
+    category: categoryReducer,
   },
 });
 
